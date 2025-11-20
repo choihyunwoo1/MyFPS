393127f2a071aac381d3b763b937e2b5853d9a9a
choihyunwoo1 <a0058765@gmail.com>
2025-11-20 17:16:55 +0900

    Robot

---
+++ Assets/MyFps/Scripts/Robot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace MyFps
+{
+    /// <summary>
+    /// 로봇을 관리하는 클래스
+    /// </summary>
+    public class Robot : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region Unity Event Method
+
+        #endregion
+
+        #region Custom Method
+
+        #endregion
+    }
+}
